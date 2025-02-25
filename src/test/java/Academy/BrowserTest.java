4929a6c3b27111f572f48f0b40f048f79f2f5998
vishnuvardhana-rmr <vishnuvardhana.rmr@gmail.com>
2025-02-25 17:10:22 +0530

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -19,10 +19,10 @@ public class BrowserTest {
 		System.out.println("Hello Guys");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		WebDriver driver =new ChromeDriver();
-		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
+		driver.get("https://vishnuvardhana-fzgedfevcngbcxav.canadacentral-01.azurewebsites.net/webapp");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+		Assert.assertTrue(text.equalsIgnoreCase("vishnuvardhanaReddy M R Learning"));
 		driver.close();
 	
 		
